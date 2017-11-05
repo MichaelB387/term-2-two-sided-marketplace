b0cb2b8df6ece302f3f3543e6860936a9623c5dc
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2017-11-05 18:53:22 +1100

    added validation to user model :name and listing model :name,:description,:price

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,8 @@
 class Listing < ApplicationRecord
     has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+    
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+    validates :name, :description, :price, presence: true
+    validates :price, numericality: { greater_than: 0 }
+    validates_attachment_presence :image
 end
