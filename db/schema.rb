c5d0bc09bbbf7b5ea6495ac88e8fcf6cbe476edf
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2017-11-05 22:32:12 +1100

    Added user_id to listings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171105072300) do
+ActiveRecord::Schema.define(version: 20171105113107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20171105072300) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
