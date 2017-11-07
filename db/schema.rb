fe53cb2570fbbf266ed76e49d3f715b075247a8f
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2017-11-07 12:11:02 +1100

    added listing_id to orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107010319) do
+ActiveRecord::Schema.define(version: 20171107011017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20171107010319) do
     t.string "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "listing_id"
   end
 
   create_table "users", force: :cascade do |t|
