ac417a42bafe96b4901ad4657315ce19925342c0
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2017-11-07 12:13:15 +1100

    link order db to user db

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ApplicationRecord
   validates :address, :city, :state, presence: true
 
   belongs_to :listing
+  belongs_to :buyer, class_name: "User"
+  belongs_to :seller, class_name: "User"
 end 
