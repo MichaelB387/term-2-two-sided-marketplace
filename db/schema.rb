08cbf9261c550979c33e1247fd837373d337f727
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2017-11-07 12:14:33 +1100

    added buyer and user id fields to orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107011017) do
+ActiveRecord::Schema.define(version: 20171107011346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20171107011017) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "listing_id"
+    t.integer "buyer_id"
+    t.integer "seller_id"
   end
 
   create_table "users", force: :cascade do |t|
