1c68e28e654e1421862c248d0c21465f3f146515
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2017-11-07 13:48:07 +1100

    rearranged links and urls

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,14 +11,9 @@ class OrdersController < ApplicationController
   end
   # GET /orders
   # GET /orders.json
-  def index
-    @orders = Order.all
-  end
 
   # GET /orders/1
   # GET /orders/1.json
-  def show
-  end
 
   # GET /orders/new
   def new
@@ -27,8 +22,6 @@ class OrdersController < ApplicationController
   end
 
   # GET /orders/1/edit
-  def edit
-  end
 
   # POST /orders
   # POST /orders.json
@@ -54,27 +47,8 @@ class OrdersController < ApplicationController
 
   # PATCH/PUT /orders/1
   # PATCH/PUT /orders/1.json
-  def update
-    respond_to do |format|
-      if @order.update(order_params)
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-        format.json { render :show, status: :ok, location: @order }
-      else
-        format.html { render :edit }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # DELETE /orders/1
   # DELETE /orders/1.json
-  def destroy
-    @order.destroy
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
