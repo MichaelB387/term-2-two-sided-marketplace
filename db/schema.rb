1ca9a078ef9447f1d723d93e5540d5d7d1fbec49
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2017-11-07 12:05:26 +1100

    created orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171105113107) do
+ActiveRecord::Schema.define(version: 20171107010319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 20171105113107) do
     t.integer "image_file_size"
     t.datetime "image_updated_at"
     t.integer "user_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
